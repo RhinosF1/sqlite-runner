8bfb96bb2efcd3053482b86cfc3da7923790275a
Pycryptor10 <47485078+Pycryptor10@users.noreply.github.com>
2021-04-23 23:09:07 +0100

    Tidy logic up (#6)
    * handle exceptions better
    
    * Update __init__.py
    
    * Update __init__.py
    
    * don't reuse queryres, manually close() dbconn for sanity

--- sqliterunner/__init__.py
+++ sqliterunner/__init__.py
@@ -2,18 +2,25 @@ from sqlite3 import connect
 
 
 def make_sql_query(dbfile, query, commit=True, fetch=False):
-    dbconn = connect(dbfile)
-    queryres = dbconn.execute(query)
+    fetchtypes = ['one', 'all', False]
+    if fetch not in fetchtypes:
+        raise ValueError(f'{fetch} is not one of {fetchtypes}')
     if commit and fetch:
-        raise Exception("You can not commit & fetch an sql query.")
-    if commit:
-        dbconn.commit()
-        dbconn.close()
-        return True
-    if fetch == 'all':
-        queryres = queryres.fetchall()
-        dbconn.close()
-    if fetch == 'one':
-        queryres = queryres.fetchone()
-        dbconn.close()
+        raise ValueError("You can not commit & fetch an sql query.")
+    if not commit and not fetch:
+        raise ValueError("One of commit or fetch must be True.")
+    with connect(dbfile) as dbconn:
+        dbconn = connect(dbfile)
+        queryexec = dbconn.execute(query)
+        if commit:
+            dbconn.commit()
+            queryres = True
+        elif fetch == 'all':
+            queryres = queryexec.fetchall()
+        elif fetch == 'one':
+            queryres = queryexec.fetchone()
+        else:
+            dbconn.close()  # for sanity
+            raise Exception("Fatal: Request not understood.")
+        dbconn.close()  # for sanity
     return queryres

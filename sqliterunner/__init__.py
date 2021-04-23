986c14cc42b6a5dc23d99c90419d7713395e5cb1
Pycryptor10 <47485078+Pycryptor10@users.noreply.github.com>
2021-04-23 22:51:18 +0100

    handle exceptions better

--- sqliterunner/__init__.py
+++ sqliterunner/__init__.py
@@ -2,18 +2,23 @@ from sqlite3 import connect
 
 
 def make_sql_query(dbfile, query, commit=True, fetch=False):
-    dbconn = connect(dbfile)
-    queryres = dbconn.execute(query)
+    fetchtypes = ['one', 'all']
+    if fetch not in fetchtypes:
+        raise ValueError(f'{fetch} is not one of {fetchtypes}')
     if commit and fetch:
-        raise Exception("You can not commit & fetch an sql query.")
-    if commit:
-        dbconn.commit()
-        dbconn.close()
-        return True
-    if fetch == 'all':
-        queryres = queryres.fetchall()
-        dbconn.close()
-    if fetch == 'one':
-        queryres = queryres.fetchone()
-        dbconn.close()
+        raise ValueError("You can not commit & fetch an sql query.")
+    if not commit and not fetch:
+        raise ValueError("One of commit or fetch must be True.")
+    with dbconn as connect(dbfile):
+        dbconn = connect(dbfile)
+        queryres = dbconn.execute(query)
+        if commit:
+            dbconn.commit()
+            queryres = True
+        elif fetch == 'all':
+            queryres = queryres.fetchall()
+        elif fetch == 'one':
+            queryres = queryres.fetchone()
+        else:
+            raise Exception("Fatal: Request not understood.")
     return queryres
